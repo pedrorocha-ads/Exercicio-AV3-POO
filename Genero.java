948f253521761b1921b417f21f46a06fcc97e780
Pedro Rocha <161658584+pedrorocha-ads@users.noreply.github.com>
2024-11-11 17:57:59 -0300

    Add files via upload
    Exercicio AV3-POO

---
+++ Genero.java
@@ -0,0 +1,43 @@
+public class Genero {
+    private int id;
+    private String descricao;
+    private String status;
+
+    public Genero(int id, String descricao, String status) {
+        this.id = id;
+        this.descricao = descricao;
+        this.status = status;
+    }
+
+    //Getter e Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    //metodos
+
+    public boolean inserir() {
+        return false;
+    }
+}
